c4541859fcdb71e1357ad461e4a4042a41e42b6b
Omar Ariza <arizaomar@gmail.com>
2020-03-29 12:41:09 -0600

    [ADD] modals

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/* CREO QUE ESTO YA NO ES NECESARIO, ERA PARA LOS MODALES. NO BORRAR (POR CUALQUIER COSA)
+$('#PostModal').on('shown.bs.modal', function () {
+    $('#PostModal').trigger('focus')
+  })*/ 
