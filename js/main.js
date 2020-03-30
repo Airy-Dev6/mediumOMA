d39e3d06c4590f41cfe2f79c997974483f5f9d1a
Omar Ariza <arizaomar@gmail.com>
2020-03-29 23:01:10 -0600

    [ADD] POST and GET functions to BD

--- js/main.js
+++ js/main.js
@@ -1,4 +1,102 @@
-/* CREO QUE ESTO YA NO ES NECESARIO, ERA PARA LOS MODALES. NO BORRAR (POR CUALQUIER COSA)
-$('#PostModal').on('shown.bs.modal', function () {
-    $('#PostModal').trigger('focus')
-  })*/ 
+const getPostData = () => {
+    let category = $("#user-category").val();
+    let title = $("#user-title").val();
+    let postUserEntry = $("#user-post").val();
+    let urlImage = $("#URL-image-post").val();
+    let author = $("#user-author").val();
+    let date = $("#user-date").val();
+    let postObject = {category, title, postUserEntry, urlImage, author, date};
+        
+    addPostToDB(postObject);
+}
+
+$("#post-button").on("click",getPostData);
+
+
+const addPostToDB = (postEntry) => {
+    $.ajax({
+        url:"https://javascript-ajax-d0ce6.firebaseio.com/omar/post/.json",
+        method:"POST",
+        data: JSON.stringify(postEntry),
+        success:(response)=>{
+            console.log(response);            
+        }
+
+    }) 
+}
+
+
+const printPostAtMain = () => {
+    $("#post-section").find("section").empty();
+    postArray.forEach((post) => {
+        let entrySlice = post.postUserEntry.slice(0,150)
+        $("#post-section").find("section").append(`
+        <a href="" class="post-modal" data-toggle="modal" data-target="#viewPost${post.key}">
+        <div class="post-card d-flex">                    
+            <div class="card-text">
+                <span class="post-category">${post.category}</span><span class="post-popular">Popular Topic</span>
+                <h2 class="post-title">${post.title}</h2>
+                <p class="post-text">${entrySlice}...</p>
+                <p class="post-author y-1">${post.author}</p>
+                <p class="post-date">${post.date}</p>
+            </div>                
+            <div class="card-image mx-md-3 mx-1">
+                <img class="c-image" src="${post.urlImage}">                
+            </div>
+        </div>
+        </a>
+        `)
+    })    
+}
+
+const printPostAtModal = () => {
+    //$("#modal-window").find("#modal-card").empty();
+    postArray.forEach((post) => {
+        $("#modal-window").find("#modal-card").append(`
+        <div class="modal fade" id="viewPost${post.key}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div>
+                        <h5 class="modal-title" id="exampleModalLabel">Entrada</h5>
+                    </div>                      
+                    <div>
+                        <p class="modal-post-author">${post.author}</p>
+                        <p class="modal-post-date">${post.date}</p>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-card-text">
+                        <img class="modal-c-image" style="align-items: center;" src="${post.urlImage}">
+                        <span class="modal-post-category">${post.category}</span><span class="post-popular">Popular Topic</span>
+                        <h2 class="modal-post-title">${post.title}</h2>
+                        <p class="modal-post-text">${post.postUserEntry}</p>
+                    </div> 
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>      
+                </div>
+            </div>
+        </div>
+        </div>        
+        `)
+    })    
+}
+
+
+const getPostFromDb = () => {
+    postArray = [];
+    $.ajax({
+        url:"https://javascript-ajax-d0ce6.firebaseio.com/omar/post/.json",
+        method:"GET",
+        success:(response)=>{
+            $.each(response,(key,value)=>{
+                postArray.push({...value,key})                
+            })
+            printPostAtMain();
+            printPostAtModal();
+        }
+    })
+}
+
+getPostFromDb()
